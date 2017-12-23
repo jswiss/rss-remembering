8d06f3702fa11c09260ef46c83d77ce4f94799e9
Joshua Swiss <swissjoshua@gmail.com>
2017-12-23 12:26:23 +0000

    Person model done for now

--- models/Person.js
+++ models/Person.js
@@ -16,7 +16,115 @@ const personSchema = new mongoose.Schema({
     trim: true,
     enum: ['deceased', 'missing']
   },
+  incidentCategory: {
+    type: String,
+    trim: true,
+    required: 'Please choose an incident category',
+    enum: ['December 2013', '1983 - 2005', '1955 - 1972', 'community violence', 'electoral violence', 'other violence']
+  },
+  bodyWitness: {
+    type: String,
+    required: 'Whether the body has been withnessed is required',
+    enum: ['yes', 'no']
+  },
+  sex: {
+    type: String,
+    required: 'sex is required',
+    enum: ['male', 'female']
+  },
+  infant: Boolean,
+  age: Number,
+  maritalStatus: {
+    type: String,
+    enum: ['married', 'single', 'separated', 'widowed']
+  },
+  originLocation: {
+    // TODO: add enums to protect data integrity here
+    state: String,
+    county: String,
+    payam: String,
+    village: String,
+  },
+  nationality: {
+    type: String,
+    enum: ['South Sudanese', 'other']
+  },
+  military: {
+    type: String,
+    enum: ['civilian', 'military/militia']
+  },
+  combatDeath: Boolean,
+  deathCategory: String,
+  deathDescription: String,
+  dateOfDeath: {
+    type: Date,
+    max: Date.now
+  },
+  locationOfDeath: {
+    // TODO: add enums to protect data integrity here
+    state: String,
+    county: String,
+    payam: String,
+    village: String,
+  },
+  othersWithInformation: [{
+    name: String,
+    telephone: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, 'Please fill in a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+  }],
+  nextOfKin: [{
+    name: String,
+    telephone: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, 'Please fill in a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+  }],
+  informer: {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    telephone: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, 'Please fill in a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    relationship: {
+      type: String,
+      required: true,
+      trim: true,
+    }
+
+  }
 });
+
+function validateEmail(email) {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?w+)+(\.\w{2,3})+$/;
+  return re.test(email);
+}
 
 // Pre-save hook in MongoDB to auto create a slug
 personSchema.pre('save', async function (next) {
