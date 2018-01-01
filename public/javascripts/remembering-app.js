d847a9ea49dcff4e7919af9a71e6260f6cccb1bc
Joshua Swiss <swissjoshua@gmail.com>
2018-01-01 16:18:33 +0000

    names cards laid out

--- public/javascripts/remembering-app.js
+++ public/javascripts/remembering-app.js
@@ -3,12 +3,12 @@ import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 import makeMap from './modules/map';
 
-autocomplete($('#address'), $('#lat'), $('#lng'));
+// autocomplete($('#address'), $('#lat'), $('#lng'));
 
-typeAhead($('.search'));
+// typeAhead($('.search'));
 
 makeMap($('#map'));
 
 // $$ from bling.js allows you to select multiple elements without having to loop over them a la jQuery
-const heartForms = $$('form.heart');
-heartForms.on('submit', ajaxHeart);
+// const heartForms = $$('form.heart');
+// heartForms.on('submit', ajaxHeart);
